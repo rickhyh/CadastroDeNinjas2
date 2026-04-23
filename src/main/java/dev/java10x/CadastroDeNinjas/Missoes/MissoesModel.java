6f6ea062a62171c60fad834b02a5342244ecb917
Icaro Carvalho <icarogacarvalho@gmail.com>
2026-04-23 09:28:43 -0300

    fix:adiciona anotaÃcoes para criacao dos construtores

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -17,8 +17,13 @@ public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column (name = "id")
     private Long id;
+
+    //@Column (name = "nome")
     private String nome;
+
+    //@Column (name = "dificuldade")
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
